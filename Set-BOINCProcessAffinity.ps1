8dcdebb9cae3a0393b0b041a279e8180d21a83fc
monahancj <monahancj@gmail.com>
2013-10-11 12:21:29 -0400

    initial add

---
+++ Set-BOINCProcessAffinity.ps1
@@ -0,0 +1,7 @@
+﻿$cpus = 1,2,4,8,16,32,64,128,255,255,255,255
+$i = 0
+#get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | select name,cpu,priorityclass,processoraffinity,userprocessortime | ft -a
+get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | % { `
+    $_.ProcessorAffinity = $cpus[$i++]
+}
+#get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | select name,cpu,priorityclass,processoraffinity,userprocessortime | ft -a
