142e6d0efae8b196bfd05b924ecdfa849503e18f
Chris Monahan <monahancj@gmail.com>
2013-10-16 11:33:00 -0400

    Added file README.  Updated BOINC affinity with PowerShell help template.

--- Set-BOINCProcessAffinity.ps1
+++ Set-BOINCProcessAffinity.ps1
@@ -1,7 +1,27 @@
-﻿$cpus = 1,2,4,8,16,32,64,128,255,255,255,255
+﻿<#
+.SYNOPSIS
+   <A brief description of the script>
+.DESCRIPTION
+   <A detailed description of the script>
+.PARAMETER <paramName>
+   <Description of script parameter>
+.EXAMPLE
+   <An example of using the script>
+.Notes
+   <Information that does not fit easily into the other sections>
+.Link
+   <Links to other Help topics and Web sites of interest>
+#>
+
+Write-Output "`n$(Get-Date)- Starting script $($PSCommandPath) with the parameters:"
+Write-Output -InputObject $PSBoundParameters | Format-Table -AutoSize
+
+$cpus = 1,2,4,8,16,32,64,128,255,255,255,255
 $i = 0
 #get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | select name,cpu,priorityclass,processoraffinity,userprocessortime | ft -a
 get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | % { `
     $_.ProcessorAffinity = $cpus[$i++]
 }
 #get-process | ? { ($_.path -match 'boinc') -and ($_.priorityclass -ne 'Normal') -and ($_.Name -notmatch "hadcm3n_6.07_windows_intelx86") } | select name,cpu,priorityclass,processoraffinity,userprocessortime | ft -a
+
+Write-Output "`n$(Get-Date)- Finished script $($PSCommandPath).`n"
